a3fb527c166979a7732a349cc580546c97bfc2ee
RocketPlumber <sb5455@nyu.edu>
2016-10-29 17:00:36 +0400

    Update piece_class.java

--- piece_class.java
+++ piece_class.java
@@ -10,9 +10,32 @@ class LinePiece{
   // The status of the block, i.e should it move or should it stay still, must be known.
   boolean moving = true;
 
-  // We initialize a line by knowing its starting x and y. These are usually going to be y = 20 and x = something between 0 and 8. A line enters the screen all flat-like.
-  LinePiece(int anchor_x, int anchor_y, Board board){
+  int orientation = 0; // For a line, there's two orientations - 0 means flat and 1 means tall.
 
+  // We initialize a line by knowing its starting x and y. These are usually going to be y = 19 and x = something between 0 and 6.
+  // We need to draw it in its original orientation (flat) by setting all occupied squares to be Active.
+  LinePiece(int anchor_x, int anchor_y, Board board){
+    // x1,y1 is forever the anchor piece
+    x1 = anchor_x;
+    y1 = anchor_y;
+
+    // The piece comes in all flat-like, so set all the y-values to be the same.
+    y2 = y1;
+    y3 = y1;
+    y4 = y1;
+
+    // Now set the x-values. Each one is one to the right of the next one
+    x2 = (x1 + 1);
+    x3 = (x2 + 1);
+    x4 = (x3 + 1);
+
+    // Now activate all the relevant pieces;
+    board[x1][y1].activate();
+    board[x2][y2].activate();
+    board[x3][y3].activate();
+    board[x4][y4].activate();
   }
+
+  
 
 }
